b4ca9b5a03cdb43a80f3337be226c0dffe5c99e6
jhowarth <jhowarth@riotgames.com>
2013-04-15 16:32:46 -0700

    version bump 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Riot Games"
 maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Installs and configures Liquibase"
-version          "0.1.0"
+version          "0.2.0"
 
 %w{ centos redhat fedora }.each do |os|
   supports os
